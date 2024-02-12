1f7748fbedded598a06deeb7abff3cf3ce3e3140
Kmag20 <owenmawira@gmail.com>
2024-02-12 05:48:09 -0800

    console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" command interpreter """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Repr the HBNBCommand class """
+    prompt = '(hbnb) '
+    def do_quit(self, line):
+        """ command to exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ command to exit the program """
+        return True
+
+    def emptyline(self):
+        """ emptyline """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
