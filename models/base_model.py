bc88db435536ae116aa3c5338101f945ec64512c
Kmag20 <owenmawira@gmail.com>
2024-02-12 05:14:57 -0800

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,23 @@
 import uuid
 import datetime
 
+
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ represents the basemodel class """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs and kwargs is not None:
+            form =  "%Y-%m-%dT%H:%M:%S.%f"
+            for arg, value in kwargs.items():
+                if arg == "__class__":
+                    continue
+                if arg == 'created_at' or arg == 'updated_at':
+                    value = datetime.datetime.strptime(value, form)
+
+                setattr(self, arg, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ prints a formal string representation of an instance """
