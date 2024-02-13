b6ce13184e8b12f0f57a6e4267efe42f57900789
Kmag20 <owenmawira@gmail.com>
2024-02-12 20:56:05 -0800

    All instances by class name

--- console.py
+++ console.py
@@ -9,6 +9,26 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from shlex import split
+import re
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -24,6 +44,37 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     ]
 
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
+
     def do_create(self, line):
         """ Creates an new instance """
         line_split = line.split(" ")
@@ -35,7 +86,8 @@ class HBNBCommand(cmd.Cmd):
             print(eval(line_split[0])().id)
             storage.save()
     def do_show(self, line):
-        """ Prints the string repr of an instance based on the class name and id """
+        """ Prints the string repr
+        of an instance based on the class name and id """
         line_split = line.split(" ")
         if line == "":
             print("** class name missing **")
